686468a16e36942849c8c0b3b17e783f460a193c
Zahharov,Sven S <s.zahharov@sutdent.fontys.nl>
2017-06-20 13:45:15 +0200

    First commit

---
+++ LPA/Domain/Uitslag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPA.Domain
+{
+    public class Uitslag
+    {
+        public int id { get; set; }
+        public int zetels { get; set; }
+        public int stemmers { get; set; }
+    }
+}
